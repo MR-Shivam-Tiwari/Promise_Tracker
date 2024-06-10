5984d2510ddc3dc6539209b47a97bb4a25648cb3
MR-Shivam-Tiwari <shivamt2023@gmail.com>
2024-06-10 11:01:05 +0530

    push

--- src/Components/Home/MainHome.jsx
+++ src/Components/Home/MainHome.jsx
@@ -367,7 +367,7 @@ function MainHome() {
                                                                         : 'https://images.unsplash.com/photo-1686548812883-9d3777f4c137?h=400&fit=crop&auto=format&dpr=2'
                                                                 }
                                                                 alt=""
-                                                            />
+                                                            /> 
                                                         </Skeleton>
                                                     </AspectRatio>
                                                 </Box>
